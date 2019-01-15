7d6cf90da94c62c2a88723617de42eaa805c9963
Davide Crestini <davide.crestini94@gmail.com>
2019-01-15 10:18:23 +0000

    small styling fix

--- src/uikit/blocks/Form/Input.js
+++ src/uikit/blocks/Form/Input.js
@@ -2,28 +2,28 @@ import styled, { css } from 'styled-components';
 import Feedback from './Feedback';
 
 const Input = styled.input`
-  width: 100%;
-  outline: none;
-  height: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  justify-content: center;
-
   ${({ theme }) => css`
+    width: 100%;
+    outline: none;
+    height: 50px;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: center;
+
     padding: 0px 5px;
     border: 2px solid ${theme.colors.border};
     border-radius: 2px;
-  `}
 
-  :focus {
-    border: 2px solid black;
-  }
-
-  :invalid {
-    ${Feedback} {
-      display: block;
+    :focus {
+      border: 2px solid ${theme.colors.primary};
     }
-  }
+
+    :invalid {
+      ${Feedback} {
+        display: block;
+      }
+    }
+  `}
 `;
 
 export default Input;
