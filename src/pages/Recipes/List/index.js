08ad4662cea678833bb110fe34f68269e42700d0
Davide Crestini <davide.crestini94@gmail.com>
2019-02-22 15:18:09 +0000

    added recipes list page

--- src/pages/Recipes/List/index.js
+++ src/pages/Recipes/List/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled, { css } from 'styled-components';
 import gql from 'graphql-tag';
 import { compose, graphql, Query } from 'react-apollo';
