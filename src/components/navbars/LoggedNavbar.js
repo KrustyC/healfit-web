4cf43c0067c27b78e919fc527ac03647227f64ef
Davide Crestini <davide.crestini94@gmail.com>
2019-03-20 17:14:32 +0000

    improved navbar

--- src/components/navbars/LoggedNavbar.js
+++ src/components/navbars/LoggedNavbar.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import { NavLink, Link } from 'react-router-dom';
 import { RootContext } from 'app/contexts/RootContext';
 import { getUserInitials } from 'helpers/user';
 import {
@@ -9,40 +11,76 @@ import {
   UikAvatar,
   UikTopBarLinkContainer,
   UikTopBarLink,
-  UikDivider,
+  UikTopBarMenuDivider,
+  UikDropdown,
+  UikDropdownItem,
 } from '@duik';
 
-const Navbar = () => {
+const Avatar = styled(UikAvatar)`
+  ${({ theme }) => css`
+    cursor: pointer;
+    div > div {
+      background: ${theme.colors.primary} !important;
+      color: white !important;
+      border-color: ${theme.colors.border} !important;
+    }
+  `}
+`;
+
+const UiKitUserDropwdown = ({ onClick }) => {
   const { authUser } = useContext(RootContext);
 
   return (
-    <UikTopBar>
-      <UikTopBarSection>
-        <UikTopBarTitle>Healfit</UikTopBarTitle>
-        <UikDivider margin vertical />
-        <UikTopBarLinkContainer>
-          <UikTopBarLink Component={NavLink} to="/dashboard">
-            Dashboard
-          </UikTopBarLink>
-          <UikTopBarLink Component={NavLink} to="/recipes">
-            Recipes
-          </UikTopBarLink>
-          <UikTopBarLink Component={NavLink} to="/meal-planner">
-            Meal Planner
-          </UikTopBarLink>
-        </UikTopBarLinkContainer>
-      </UikTopBarSection>
-
-      <UikTopBarSection>
-        <UikAvatar
-          avatarPlaceholder={{
-            content: getUserInitials(authUser),
-            color: 'blue',
-          }}
-        />
-      </UikTopBarSection>
-    </UikTopBar>
+    <Avatar
+      onClick={onClick}
+      avatarPlaceholder={{
+        content: getUserInitials(authUser),
+      }}
+    >
+      CustomDisplayComponent
+    </Avatar>
   );
 };
+
+UiKitUserDropwdown.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+const Navbar = () => (
+  <UikTopBar>
+    <UikTopBarSection>
+      <UikTopBarTitle>Healfit</UikTopBarTitle>
+      <UikTopBarMenuDivider />
+      <UikTopBarLinkContainer>
+        <UikTopBarLink Component={NavLink} to="/dashboard">
+          Dashboard
+        </UikTopBarLink>
+        <UikTopBarLink Component={NavLink} to="/recipes">
+          Recipes
+        </UikTopBarLink>
+        <UikTopBarLink Component={NavLink} to="/meal-planner">
+          Meal Planner
+        </UikTopBarLink>
+      </UikTopBarLinkContainer>
+    </UikTopBarSection>
+
+    <UikTopBarSection>
+      <UikDropdown
+        position="bottomaRight"
+        DisplayComponent={UiKitUserDropwdown}
+      >
+        <UikDropdownItem Component={Link} to="/me/profile">
+          Profile
+        </UikDropdownItem>
+        <UikDropdownItem Component={Link} to="/me/settings">
+          Account Settings
+        </UikDropdownItem>
+        <UikDropdownItem Component={Link} to="/auth/logout">
+          Logout
+        </UikDropdownItem>
+      </UikDropdown>
+    </UikTopBarSection>
+  </UikTopBar>
+);
 
 export default Navbar;
