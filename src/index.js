d780e3ce896331514261ab0e82dbdba475ce4e9b
Davide <davide.crestini94@gmail.com>
2019-04-02 00:03:44 +0100

    started implementing service woroker

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import bugsnag from '@bugsnag/js';
 import bugsnagReact from '@bugsnag/plugin-react';
 import './duik/styles.css';
+import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 const bugsnagClient = bugsnag(process.env.BUGSNAG_API_KEY);
 bugsnagClient.use(bugsnagReact, React);
+registerServiceWorker();
 
 const ErrorBoundary = bugsnagClient.getPlugin('react');
 
