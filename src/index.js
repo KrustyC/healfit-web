7da37317dec525490ee65529365dfea51f4f50ac
Davide Crestini <davide.crestini94@gmail.com>
2019-04-02 10:25:44 +0100

    re added index which I stupidely removed earlier on

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import bugsnag from '@bugsnag/js';
+import bugsnagReact from '@bugsnag/plugin-react';
+import './duik/styles.css';
+import App from './App';
+
+const bugsnagClient = bugsnag(process.env.BUGSNAG_API_KEY);
+bugsnagClient.use(bugsnagReact, React);
+
+const ErrorBoundary = bugsnagClient.getPlugin('react');
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+);
